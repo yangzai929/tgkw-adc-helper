f4901946479857dc33133608a6d050b83f3ee4f1
ou.阳 <755792463@qq.com>
2025-10-13 14:16:57 +0800

    init

--- src/Helper/ApiResponseHelper.php
+++ src/Helper/ApiResponseHelper.php
@@ -2,33 +2,34 @@
 
 namespace TgkwAdc\Helper;
 
-use Hyperf\Context\TgkwAdclicationContext;
+use Hyperf\Context\ApplicationContext;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Psr\Http\Message\ResponseInterface as Psr7ResponseInterface;
 
 class ApiResponseHelper
 {
-    public static function success($data = null, $message = 'success', $code = 200): Psr7ResponseInterface
+    public static function success($data = null, $message = 'success', $code = 0,$httpStatusCode = 200): Psr7ResponseInterface
     {
-        $response = TgkwAdclicationContext::getContainer()->get(ResponseInterface::class);
+        $response = ApplicationContext::getContainer()->get(ResponseInterface::class);
 
         return $response->json([
             'code' => $code,
             'message' => $message,
             'data' => $data,
             'timestamp' => time(),
-        ]);
+        ])->withStatus($httpStatusCode);
     }
 
-    public static function error($message = 'error', $data = null, $code = 400): Psr7ResponseInterface
+    public static function error($message = 'error',$error=null, $data = null, $code = 400,$httpStatusCode = 200): Psr7ResponseInterface
     {
-        $response = TgkwAdclicationContext::getContainer()->get(ResponseInterface::class);
+        $response = ApplicationContext::getContainer()->get(ResponseInterface::class);
 
         return $response->json([
             'code' => $code,
             'message' => $message,
             'data' => $data,
+            'error' => $error,
             'timestamp' => time(),
-        ]);
+        ])->withStatus($httpStatusCode);
     }
 }
