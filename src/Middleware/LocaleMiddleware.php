4726a489b7ac25e80ac6f417c56e7ce12f5cd4ec
ou.阳 <755792463@qq.com>
2025-10-18 16:41:09 +0800

    chore(locale): 移除调试输出语句

--- src/Middleware/LocaleMiddleware.php
+++ src/Middleware/LocaleMiddleware.php
@@ -33,7 +33,6 @@ class LocaleMiddleware implements MiddlewareInterface
             $container = ApplicationContext::getContainer();
             $this->translator = $container->get(TranslatorInterface::class);
         }
-        echo '44444444444444444444444444444444444444444';
 
         // 设置到翻译器
         $this->translator->setLocale($locale);
